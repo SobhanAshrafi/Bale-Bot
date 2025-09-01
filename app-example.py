c3171b6309c16c1db4328092f8e478f5d82e5828
Sobhan Ashrafi <74056968+SobhanAshrafi@users.noreply.github.com>
2025-09-01 10:24:33 +0330

    Update and rename cu.py to app-example.py

--- app-example.py
+++ app-example.py
@@ -9,21 +9,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logging.getLogger(__name__)
 
 
-app = App(token = "811876770:Qnwb52SDgE14QdiWDUKziKm4kvjPmBMcjsofyuNo")
+app = App(token = "Token")
 
 
 
 
 @app.updates_controller.message_handler('/start')
 def start_command(context):
-    text = 'سلام به ربات \یام ناشناس خوش آمدید'
-    app.bot.send_text(chat_id=658937530, text=text)
+    text = 'سلام به ربات *** خوش آمدید'
+    app.bot.send_text(chat_id="chat_id", text=text)
     
 
 @app.updates_controller.message_handler('echo.+',use_re_filter=True)
 def echo(context):
 
-    app.bot.send_text(chat_id=658937530,text=context['text'][4:])    
+    app.bot.send_text(chat_id="chat_id",text=context['text'][4:])    
 
 
 
@@ -32,5 +32,6 @@ def echo(context):
 
 
 app.run()
+
 
 
